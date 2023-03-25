f9a1f10759a02e63180937ddfc81f0db2c3fdf92
xtrystin <kamsil12@gmail.com>
2023-03-25 21:26:58 +0100

    Removed authorization for test endpoint

--- src/AuthService/Controllers/WeatherForecastController.cs
+++ src/AuthService/Controllers/WeatherForecastController.cs
@@ -5,7 +5,6 @@ namespace AuthService.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Authorize(Roles = "moderator")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
