149953c267034d83660a1cb0c846bf244953022c
xtrystin <kamsil12@gmail.com>
2023-09-09 18:31:25 +0200

    Adds weather forecast page with a map to select location and a graph with tooltip to display weather info

---
+++ src/UI/WebAssemblyUI/Models/GeoLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAssemblyUI.Models;
+
+public class GeoLocation
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Ipv4 { get; set; }
+    public string City { get; set; }
+}
---
+++ src/UI/WebAssemblyUI/Models/WeatherForecast.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebAssemblyUI.Models;
+
+public class WeatherForecast
+{
+    [JsonPropertyName("daily")]
+    public DailyWeather DailyWeather { get; set; }
+
+    //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class DailyWeather
+{
+    [JsonPropertyName("time")]
+    public List<DateTime> Dates { get; set; }
+    [JsonPropertyName("temperature_2m_min")]
+    public List<double> MinTemperatures { get; set; }
+    [JsonPropertyName("temperature_2m_max")]
+    public List<double> MaxTemperatures { get; set; }
+    [JsonPropertyName("temperature_2m_mean")]
+    public List<double> MeanTemperatures { get; set; }
+
+    [JsonPropertyName("rain_sum")]
+    public List<double> RainSum { get; set; }
+    [JsonPropertyName("snowfall_sum")]
+    public List<double> SnowfallSum { get; set; }
+    [JsonPropertyName("precipitation_hours")]
+    public List<double> PrecipitationHours { get; set; }
+    [JsonPropertyName("precipitation_probability_mean")]
+    public List<int?> PrecipitationProbabilityMean { get; set; }
+}
+
+class DataItem
+{
+    public DateTime Date { get; set; }
+    public double MinTemperature { get; set; }
+    public double MaxTemperature { get; set; }
+    public double MeanTemperature { get; set; }
+
+    public double RainSum { get; set; }
+    public double SnowfallSum { get; set; }
+    public double PrecipitationHours { get; set; }
+    public int? PrecipitationProbabilityMean { get; set; }
+}
