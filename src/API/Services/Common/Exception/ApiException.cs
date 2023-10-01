9e58aab267689cc7ccc5b8e995935ec89353a725
xtrystin <kamsil12@gmail.com>
2023-10-01 13:07:12 +0200

    Add exception handler middleware to Common library

---
+++ src/API/Services/Common/Exception/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Common.Exception;
+public class ApiException : System.Exception
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public ApiException(HttpStatusCode statusCode, string message) : base(message)
+	{
+        StatusCode = statusCode;
+    }
+    
+}
---
+++ src/API/Services/Common/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using Common.Exception;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Common.Middleware;
+
+public class ExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (System.Exception ex)
+        {
+            await HandleException(ex, context.Response);
+        }
+    }
+
+    private async Task HandleException(System.Exception ex, HttpResponse response)
+    {
+        try
+        {
+            if (ex is ApiException)
+            {
+                ApiException apiEx = ex as ApiException;
+                response.StatusCode = (int)apiEx.StatusCode;
+
+                response.WriteAsync(apiEx.Message);
+                response.ContentType = "text/plain";
+            }
+            else
+            {
+                // todo: write exception to logs
+                // if not production
+                     //throw ex;
+                response.StatusCode = 500;
+                response.ContentType = "text/plain";
+                await response.WriteAsync("Unhandled exception occurred");                       
+            }
+        }
+        catch (System.Exception responseWritingException)
+        {
+            // todo: log this and outer exception
+            // if not production
+            //throw ex;
+        }
+
+    }
+}
+
+public static class ExceptionHandlerMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        => builder.UseMiddleware<ExceptionHandlerMiddleware>();
+}
