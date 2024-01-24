2130d0a974ccac2f0aa1245ce949d2e1da442c62
xtrystin <kamsil12@gmail.com>
2024-01-24 01:06:13 +0100

    Changed weatherforecast api contract in UI
    Discards received entries with null temperatures

--- src/UI/WebAssemblyUI/Models/WeatherForecast.cs
+++ src/UI/WebAssemblyUI/Models/WeatherForecast.cs
@@ -15,18 +15,18 @@ public class DailyWeather
     [JsonPropertyName("time")]
     public List<DateTime> Dates { get; set; }
     [JsonPropertyName("temperature_2m_min")]
-    public List<double> MinTemperatures { get; set; }
+    public List<double?> MinTemperatures { get; set; }
     [JsonPropertyName("temperature_2m_max")]
-    public List<double> MaxTemperatures { get; set; }
+    public List<double?> MaxTemperatures { get; set; }
     [JsonPropertyName("temperature_2m_mean")]
-    public List<double> MeanTemperatures { get; set; }
+    public List<double?> MeanTemperatures { get; set; }
 
     [JsonPropertyName("rain_sum")]
-    public List<double> RainSum { get; set; }
+    public List<double?> RainSum { get; set; }
     [JsonPropertyName("snowfall_sum")]
-    public List<double> SnowfallSum { get; set; }
+    public List<double?> SnowfallSum { get; set; }
     [JsonPropertyName("precipitation_hours")]
-    public List<double> PrecipitationHours { get; set; }
+    public List<double?> PrecipitationHours { get; set; }
     [JsonPropertyName("precipitation_probability_mean")]
     public List<int?> PrecipitationProbabilityMean { get; set; }
 }
@@ -34,12 +34,12 @@ public class DailyWeather
 class DataItem
 {
     public DateTime Date { get; set; }
-    public double MinTemperature { get; set; }
-    public double MaxTemperature { get; set; }
-    public double MeanTemperature { get; set; }
+    public double? MinTemperature { get; set; }
+    public double? MaxTemperature { get; set; }
+    public double? MeanTemperature { get; set; }
 
-    public double RainSum { get; set; }
-    public double SnowfallSum { get; set; }
-    public double PrecipitationHours { get; set; }
+    public double? RainSum { get; set; }
+    public double? SnowfallSum { get; set; }
+    public double? PrecipitationHours { get; set; }
     public int? PrecipitationProbabilityMean { get; set; }
 }
